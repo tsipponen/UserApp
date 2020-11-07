b51268aa11a021eb3458d73333e91f874e15d450
Tino Santeri Juhani Sipponen <tinosipponen@gmail.com>
2020-11-07 10:32:50 +0200

    Added gitignore and changed db.js

--- db.js
+++ db.js
@@ -1,2 +1,11 @@
-var mongoose = require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/users", { useNewUrlParser: true });
+//var mongoose = require("mongoose");
+//mongoose.connect("mongodb://127.0.0.1:27017/users", { useNewUrlParser: true });
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://tiny:<password>@tinycluster.6gq7r.azure.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {userNewUrlParser:true});
+
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    //perform actions to the collection objects
+    client.close();
+})
