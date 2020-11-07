22f47bf358d4870b889b4d8696a05b2dd5db4ce5
Tino Santeri Juhani Sipponen <tinosipponen@gmail.com>
2020-11-07 13:01:42 +0200

    Added asking the user for username, pw and dbname

--- auth.js
+++ auth.js
@@ -1,26 +1,47 @@
 'use strict'
 
 let username;
-let password;
+let pw;
+let dbname;
 
-let dbName;
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
 const userName = () => {
-    return new Promise((resolve, reject) =>{
+    return new Promise((resolve, reject) => {
         readline.question('Enter your username: ', (username) => {
-            console.log(`Username set to ${username}`);
-            resolve();
-        });
-    });
-};
+            resolve(username)
+        })
+    })
+}
+
+const passWord = () => {
+    return new Promise((resolve, reject) => {
+        readline.question('Enter your password: ', (password) => {
+            resolve(password)
+        })
+    })
+}
+
+const dbName = () => {
+    return new Promise((resolve, reject) => {
+        readline.question('Enter the database name you want to connect to: ', (dbname) => {
+            resolve(dbname)
+        })
+    })
+}
 
 const main = async () =>{
-    await userName();
+    username = await userName();
+    pw = await passWord();
+    dbname = await dbName();
+
     readline.close();
+    console.log(`Username: ${username}`);
+    console.log(`PW: ${pw}`);
+    console.log(`Database: ${dbname}`);
 }
 
 main();
